504c303bb4007275cc045492340ceb5d4bdbd4bd
AlgoWebBot <shimulzahan636@gmail.com>
2024-11-15 00:16:38 +0600

    setup routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import Login from './Pages/Login.jsx'
+import Register from './Pages/Register.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Outlet />,
+    errorElement: <h1>404, Page not found</h1>,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div className='dark'>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+  </div>
 )
